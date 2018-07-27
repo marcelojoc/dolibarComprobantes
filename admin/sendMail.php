70cb216661ab4893e98332fd0cdfc5206646a06e
Marcelo tms <marcelojoc@gmail.com>
2018-07-27 16:05:31 -0300

    formulario de envio de comprobante .. etapa de prueba

--- admin/sendMail.php
+++ admin/sendMail.php
@@ -33,12 +33,12 @@ $langs->load("other");
 $langs->load("errors");
 
 $action=GETPOST('action','alpha');
-$comp=GETPOST('id','alpha');
+$comp=GETPOST('comp','alpha');
 
 if (! $user->admin) accessforbidden();
 
 
-	// var_dump($comp);
+	// var_dump(DOL_DOCUMENT_ROOT);
 
 	if($comp != ''){    //  si envio datos de id de comprobante
 
@@ -51,98 +51,89 @@ if (! $user->admin) accessforbidden();
 
 			$comprobante->dataFactura(); // busco todos los datos del comprobante;
 
-				$id=0;
-				$actiontypecode='';     // Not an event for agenda
-				$trigger_name='$trackid';       // Disable triggers
-				$paramname='id';
-				$mode='emailfortest';
-				$trackid='comprobante';
-				include DOL_DOCUMENT_ROOT.'/core/actions_sendmails.inc.php';
+			$id=0;
+			$actiontypecode='';     // Not an event for agenda
+			$trigger_name='$trackid';       // Disable triggers
+			$paramname='id';
+			$mode='emailfortest';
+			$trackid='comprobante';
+			include DOL_DOCUMENT_ROOT.'/core/actions_sendmails.inc.php';
+			/*
+			* View
+			*/
+			$wikihelp='EN:Setup_EMails|FR:Paramétrage_EMails|ES:Formulario envio de comprobantes';
+			llxHeader('','Formulario envio de comprobantes',$wikihelp);
+			$head = email_admin_prepare_head();
 
-				/*
-				* View
-				*/
+			print load_fiche_titre('Formulario de envio de comprobantes','','');
 
+			// Cree l'objet formulaire mail
+			include_once DOL_DOCUMENT_ROOT.'/core/class/html.formmail.class.php';
+			$formmail = new FormMail($db);
+			$formmail->trackid= $trackid;
+			$formmail->fromname = (isset($_POST['fromname'])?$_POST['fromname']:$conf->global->MAIN_MAIL_EMAIL_FROM);
+			$formmail->frommail = (isset($_POST['frommail'])?$_POST['frommail']:$conf->global->MAIN_MAIL_EMAIL_FROM);
+			$formmail->fromid= $user->id;
+			$formmail->fromalsorobot=1;
+			$formmail->fromtype= (GETPOST('fromtype')?GETPOST('fromtype'):(!empty($conf->global->MAIN_MAIL_DEFAULT_FROMTYPE)?$conf->global->MAIN_MAIL_DEFAULT_FROMTYPE:'user'));
 
-				$wikihelp='EN:Setup_EMails|FR:Paramétrage_EMails|ES:Formulario envio de comprobantes';
-				llxHeader('','Formulario envio de comprobantes',$wikihelp);
-				$head = email_admin_prepare_head();
+			$formmail->withfromreadonly=1;
+			$formmail->withsubstit=0;
+			$formmail->withfrom=1;
+			// $formmail->witherrorsto=1;
+			$formmail->withto=(! empty($_POST['sendto'])?$_POST['sendto']:($comprobante->emailCliente));
+			$formmail->withtocc=(! empty($_POST['sendtocc'])?$_POST['sendtocc']:1);       // ! empty to keep field if empty
+			$formmail->withtoccc=(! empty($_POST['sendtoccc'])?$_POST['sendtoccc']:1);    // ! empty to keep field if empty
+			$formmail->withtopic=(isset($_POST['subject'])?$_POST['subject']:'Comprobante de pago '.$comprobante->referenciaComprobante);
 
-				print load_fiche_titre('Formulario de envio de comprobantes','','');
+			$formmail->withtopicreadonly=0;
+			$formmail->withfile=2;
+			// este esw el cuerpo del mensaje
 
-				// Cree l'objet formulaire mail
-				include_once DOL_DOCUMENT_ROOT.'/core/class/html.formmail.class.php';
-				$formmail = new FormMail($db);
-				$formmail->trackid= $trackid;
-				$formmail->fromname = $conf->global->MAIN_MAIL_EMAIL_FROM;
-				$formmail->frommail = $conf->global->MAIN_MAIL_EMAIL_FROM;
-				$formmail->fromid= $user->id;
-				$formmail->fromalsorobot=1;
-				$formmail->fromtype=(GETPOST('fromtype')?GETPOST('fromtype'):(!empty($conf->global->MAIN_MAIL_DEFAULT_FROMTYPE)?$conf->global->MAIN_MAIL_DEFAULT_FROMTYPE:'user'));
+			$formmail->withbody='__(Hello)__,<br><br>
 
-				$formmail->withfromreadonly=1;
-				$formmail->withsubstit=0;
-				$formmail->withfrom=1;
-				// $formmail->witherrorsto=1;
-				$formmail->withto= $comprobante->emailCliente;     // ! empty to keep field if empty
-				$formmail->withtoccc=(! empty($_POST['sendtoccc'])?$_POST['sendtoccc']:0);    // ! empty to keep field if empty
-				$formmail->withtopic=(isset($_POST['subject'])?$_POST['subject']:'comprobante 55555555');
-				$formmail->withtopicreadonly=0;
-				$formmail->withfile=2;
-		$formmail->withbody='__(Hello)__,<br><br>
-
-		Estimado __MEMBER_FIRSTNAME__ <br><br>
-		
-			Envio comprobante de pago para la factura<br><br>
+			Estimado '.$comprobante->nombreCliente.' <br><br>
 			
-		__(Sincerely)__<br><br>'.$conf->global->MAIN_INFO_SOCIETE_NOM;
+				Envio comprobante de pago para la factura '.$comprobante->referenciaFactura.' <br><br>
+				
+			__(Sincerely)__<br><br>'.$conf->global->MAIN_INFO_SOCIETE_NOM;
 
-
-
-		$formmail->withbodyreadonly=0;
-		$formmail->withcancel=1;
-		$formmail->withdeliveryreceipt=1;
-		$formmail->ckeditortoolbar='dolibarr_mailings';
-		// Tableau des substitutions
-		
-		// Tableau des parametres complementaires du post
-		$formmail->param["action"]="send";
-		$formmail->param["models"]="body";
-		$formmail->param["mailid"]=0;
-		$formmail->param["returnurl"]=$_SERVER["PHP_SELF"];
-		$formmail->param['fileinit'] = 'C:/wamp/www/dolibar_local/documents/facture/FA1807-13811/FA1807-13811.pdf';
-
-
-
-		$formmail->param['action'] = 'send';
-		// $formmail->param['models'] = 'facture_send';
-		$formmail->param['models_id']=GETPOST('modelmailselected','int');
-		$formmail->param['id'] = '14150';
-		$formmail->param['returnurl'] = DOL_MAIN_URL_ROOT . '/compta/paiement/card.php?id=' . '14150';
-		$file= 'C:\wamp\www\dolibar_local\documents\comprobantes\PAY1807-14073';
-		$formmail->param['fileinit'] = array($file,);
-
-        
-		// Init list of files
-		if (GETPOST("mode")=='init')
-		{
+			$formmail->withbodyreadonly=0;
+			$formmail->withcancel=1;
+			$formmail->withdeliveryreceipt=1;
+			$formmail->ckeditortoolbar='dolibarr_mailings';
+			// Tableau des substitution
+			// Tableau des parametres complementaires du post
+			$formmail->param["action"]="send";
+			$formmail->param["models"]="body";
+			$formmail->param["mailid"]=0;
+			$formmail->param["returnurl"]=$_SERVER["PHP_SELF"].'?comp='. $comp;
+			$file= DOL_DATA_ROOT.'/comprobantes/'.$comprobante->referenciaComprobante.'/'.$comprobante->referenciaComprobante.'.pdf';
+			$formmail->param['fileinit'] = array($file);		
+			$formmail->param['id'] = $comp;
+			
 			$formmail->clear_attached_files();
-		}
-
-		$formmail->clear_attached_files();
 
 
-		$formmail->add_attached_files($file, basename($file), 'application/pdf');
-		print $formmail->get_form();
+			$formmail->add_attached_files($file, basename($file), 'application/pdf');
+			print $formmail->get_form();
 
-		
-		dol_fiche_end();
+			
+			dol_fiche_end();
 
 
 		}else{
 
 
 			// el numero de id del comprobante no existe 
+			$wikihelp='EN:Setup_EMails|FR:Paramétrage_EMails|ES:Formulario envio de comprobantes';
+			llxHeader('','Formulario envio de comprobantes',$wikihelp);
+			$head = email_admin_prepare_head();
+
+			print load_fiche_titre('Formulario de envio de comprobantes','','');
+
+			print '<b>EL NUMERO DE COMPROBANTE NO EXISTE </b><br>';
+			print '<b><a href="'.DOL_URL_ROOT.'/compta/paiement/card.php?id='.$comp.'">Volver</a></b>';
 
 
 		}
@@ -153,20 +144,38 @@ if (! $user->admin) accessforbidden();
 	}else{  // Si no mando nada
 
 
+		$wikihelp='EN:Setup_EMails|FR:Paramétrage_EMails|ES:Formulario envio de comprobantes';
+		llxHeader('','Formulario envio de comprobantes',$wikihelp);
+		$head = email_admin_prepare_head();
 
+		print load_fiche_titre('Formulario de envio de comprobantes','','');
+
+		print '<b>NO HAY DATOS ENVIADOS PARA PROCESAR</b>';
 		
+		// var_dump($conf->global->MAIN_MAIL_EMAIL_FROM);
+
+
+
 
 
 	}
 
 
 
+	// var_dump($_POST);
+	// var_dump($_SERVER["PHP_SELF"]);
+	
+
+	// $comprobante = new getComprobantes($db);
+				
+	// $valorComprobante= $comprobante->setIdComprobante($comp);
 
 
+	    //  si el resultado de setear el comprobante es favorable puedo desplegar el formulario
 
+		
 
-
-
+	
 	
 	
 		
